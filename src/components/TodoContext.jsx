70da81681a7999ff57c7b343742a0d1dbda9b386
Hansaem Park <sammita@Hansaems-MacBook-Pro.local>
2022-03-13 10:57:38 +0900

    Prep for functionality

---
+++ src/components/TodoContext.jsx
@@ -0,0 +1,81 @@
+import React, { useReducer, createContext, useContext, useRef } from "react";
+
+const initalTodos = [
+  {
+    id: 1,
+    text: "Create a project",
+    done: true,
+  },
+  {
+    id: 2,
+    text: "Style a component",
+    done: true,
+  },
+  {
+    id: 3,
+    text: "Create a Context",
+    done: false,
+  },
+  {
+    id: 4,
+    text: "Implement a functionality",
+    done: false,
+  },
+];
+
+function todoReducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return state.concat(action.todo);
+    case "UPDATE":
+      return state.map((todo) =>
+        todo.id === action.id ? { ...todo, done: !todo.done } : todo
+      );
+    case "REMOVE":
+      return state.filter((todo) => todo.id !== action.id);
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
+// Component Optimization
+const TodoStateContext = createContext();
+const TodoDispatchContext = createContext();
+const TodoNextIdContext = createContext();
+
+export function TodoProvider({ children }) {
+  const [state, dispatch] = useReducer(todoReducer, initalTodos);
+  const nextId = useRef(5);
+  return (
+    <TodoStateContext.Provider value={state}>
+      <TodoDispatchContext.Provider value={dispatch}>
+        <TodoNextIdContext value={nextId}>{children}</TodoNextIdContext>
+      </TodoDispatchContext.Provider>
+    </TodoStateContext.Provider>
+  );
+}
+
+// Custom Hook
+
+// Context for STATE
+export function useTodoState() {
+  const context = useContext(TodoStateContext);
+  if (!context) {
+    throw new Error("Cannot find TodoProvider");
+  }
+}
+
+// Context for DISPATCH
+export function useTodoDispatch() {
+  const context = useContext(TodoDispatchContext);
+  if (!context) {
+    throw new Error("Cannot find TodoProvider");
+  }
+}
+
+export function useTodoNextId() {
+  const context = useContext(TodoNextIdContext);
+  if (!context) {
+    throw new Error("Cannot find TodoProvider");
+  }
+}
