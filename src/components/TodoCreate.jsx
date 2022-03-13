3ac52907d16a65a4e84f81e1b1b3d8f01e54b331
Hansaem Park <sammita@Hansaems-MacBook-Pro.local>
2022-03-13 10:11:10 +0900

    Final UI

--- src/components/TodoCreate.jsx
+++ src/components/TodoCreate.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled, { css } from "styled-components";
 import { MdAdd } from "react-icons/md";
 
@@ -30,13 +30,68 @@ const CircleButton = styled.button`
 
   border: none;
   outline: none;
+
+  transition: 0.125s all ease-in;
+
+  ${(props) =>
+    props.open &&
+    css`
+      background: #ff6b6b;
+      &:hover {
+        background: #ff8787;
+      }
+      &:active {
+        background: #fa5252;
+      }
+      transform: translate(-50%, 50%) rotate(45deg);
+    `}
+`;
+
+const InsertFormPositioner = styled.div`
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  position: absolute;
+`;
+
+const InsertForm = styled.div`
+  background: #f8f9fa;
+  padding: 32px;
+  padding-bottom: 72px;
+  border-bottom-left-radius: 16px;
+  border-bottom-right-radius: 16px;
+  border-top: 1px solid #e9ecef;
+`;
+
+const Input = styled.input`
+  padding: 12px;
+  border-radius: 4px;
+  border: 1px solid #dee3e6;
+  width: 100%;
+  outline: none;
+  font-size: 18px;
+  box-sizing: border-box;
 `;
 
 const TodoCreate = () => {
+  const [open, setOpen] = useState(false);
+  const onToggle = () => setOpen(!open);
   return (
-    <CircleButton>
-      <MdAdd />
-    </CircleButton>
+    <>
+      {open && (
+        <InsertFormPositioner>
+          <InsertForm>
+            <Input
+              placeholder="Hit the ENTER after typing a thing to do"
+              autoFocus
+            />
+          </InsertForm>
+        </InsertFormPositioner>
+      )}
+      <CircleButton onClick={onToggle} open={open}>
+        <MdAdd />
+      </CircleButton>
+    </>
   );
 };
 
